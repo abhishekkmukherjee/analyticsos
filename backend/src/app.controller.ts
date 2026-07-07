114a28231718265b2c056f6bede1809ee5396ca4
Abhishek Mukherjee <mukherjeeabhishek207@gmail.com>
2026-07-08 00:48:00 +0530

    WIP: config service wiring for health check

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(){
+  getHello()
+  {
     return this.appService.getHello();
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { version } from 'os';
+
 
 @Injectable()
 export class AppService {
